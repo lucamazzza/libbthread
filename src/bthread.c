65005130567341e8a42c300d61780a8db31a97df
Luca Mazza <luca.mazza@mazluc.ch>
2025-10-11 14:46:53 +0200

    feat: is thread in zombie state

--- src/bthread.c
+++ src/bthread.c
@@ -13,6 +13,29 @@ __bthread_scheduler *bthread_get_scheduler(void) {
 }
 
 int bthread_is_thread_zombie_state(bthread_t thread, void **retval) {
+    int done = 0;
+    bthread_begin_atomic_execution();
+    __bthread_scheduler *s = bthread_get_scheduler();
+    tqueue_t start = bthread_get_queue_starting_at_thread(thread);
+    if (start) {
+        __bthread_private *tp = (__bthread_private*) tqueue_get_data(start);
+        if (tp->tid == thread && tp->state == __BTHREAD_ZOMBIE) {
+            if (retval) {
+                *retval = tp->retval;
+            }
+            if (tp->stack) {
+                free((void*) tp->stack);
+                tp->stack = 0;
+            }
+            if (s->cur == start) {
+                s->cur = tqueue_at_offset(start, 1);
+            }
+            s->queue = tqueue_pop(&start);
+            done = 1;
+        }
+    }
+    bthread_end_atomic_execution();
+    return done;
 }
 
 int bthread_create(bthread_t *thread, const bthread_attr_t *attr, void *(*start_routine)(void *), void *arg) {
@@ -21,6 +44,7 @@ int bthread_create(bthread_t *thread, const bthread_attr_t *attr, void *(*start_
     t->tid = ++s->cur_tid;
     t->body = start_routine;
     t->arg = arg;
+    t->attr = *attr;
     t->state = __BTHREAD_READY;
     t->stack = 0;
     tqueue_enqueue(&s->queue, t);
